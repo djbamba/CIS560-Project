4b57ab716d0d5e55f9b8b6e2bdc88d56ec0bec9a
Daniel Bamba <djbamba@ksu.edu>
2016-11-30 02:53:36 -0600

    Added image tag to games/game template. Added some properties to prevent auto-restarting while populating tables. All but 61 games have image urls.

--- src/main/java/com/dj/Application.java
+++ src/main/java/com/dj/Application.java
@@ -2,6 +2,7 @@ package com.dj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by DJ on 11/9/16.
