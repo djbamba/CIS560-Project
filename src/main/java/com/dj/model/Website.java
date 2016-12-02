419fb5d8f126d9adf4f499fd51277d83f86fa07a
Daniel Bamba <djbamba@ksu.edu>
2016-12-01 23:57:42 -0600

    Changed up persistance annotations.

--- src/main/java/com/dj/model/Website.java
+++ src/main/java/com/dj/model/Website.java
@@ -2,6 +2,7 @@ package com.dj.model;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,8 +21,10 @@ public class Website {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
+	@Column(name = "name", unique = true, nullable = false)
 	private String name;
 	
+	@Column(name = "url", unique = true, nullable = false)
 	private String url;
 	
 	@ManyToMany(mappedBy = "websites")
