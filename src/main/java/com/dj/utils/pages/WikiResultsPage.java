a63f0cf79249620f94692a558ec974072ea039bb
Daniel Bamba <djbamba@ksu.edu>
2016-12-05 23:52:20 -0600

    Lost it all.... It's all gone... Gone. Gone. Gone.

--- src/main/java/com/dj/utils/pages/WikiResultsPage.java
+++ src/main/java/com/dj/utils/pages/WikiResultsPage.java
@@ -32,28 +32,19 @@ public class WikiResultsPage extends WikiPage {
 	private WebElement infoBlock;
 	
 	@FindBy(xpath = "//table[@class='infobox hproduct']//tr[3]/td/a[1]")
-	private List<WebElement> developer;
+	private WebElement developer;
 	
 	@FindBy(xpath = "//table[@class='infobox hproduct']//tr[contains(.,'Publisher(s)')]/td/*[not(self::a[contains(.,'‹See Tfd›')])][1]")
-	private List<WebElement> publisher;
+	private WebElement publisher;
 	
-	/**
-	 * tony hawk: id('mw-content-text')/x:table[2]/x:tbody/x:tr[5]/x:td
-	 * tony hawk ps3: id('mw-content-text')/x:table/x:tbody/x:tr[5]/x:td/text()
-	 * tekken 3: id('mw-content-text')/x:table[1]/x:tbody/x:tr[7]/x:td
-	 * perfect dark: id('mw-content-text')/x:table[1]/x:tbody/x:tr[6]/x:td
-	 *
-	 * attempt: //table[@class='infobox hproduct']//tbody//tr[contains(.,'Designer(s)')]/td
-	 * ok: //div[@id='mw-content-text']/table/tbody/x:tr[contains(.,'Designer(s)')]/td//text()[not(contains(.,'('))]
-	 */
 	@FindBy(xpath = "//div[@id='mw-content-text']/table/tbody/x:tr[contains(.,'Designer(s)')]/td//text()[not(contains(.,'('))]")
-	private List<WebElement> designer;
+	private WebElement designer;
 	
 	@FindBy(xpath = "//span[@class='nowraplinks']/*[not(self::small)]")
-	private List<WebElement> platforms;
+	private WebElement platforms;
 	
 	@FindBy(xpath = "//table[@class='infobox hproduct']//tr[contains(.,'Genre(s)')]/td/a")
-	private List<WebElement> genres;
+	private WebElement genres;
 	
 	@FindBy(xpath = "//table[@class='infobox hproduct']//tr[2]/td/a/img")
 	private WebElement image;
@@ -71,17 +62,14 @@ public class WikiResultsPage extends WikiPage {
 		super(driver, null);
 	}
 	
-	/**
-	 * Method to test the information block's contents
-	 *
-	 * @return information block's text contents
-	 */
+
 	public String shredBlock() {
 		
 		return infoBlock.getText();
 	}
 	
 	public Publisher getPublisher() {
+		/*
 		try {
 			
 			return new Publisher(publisher.get(0).getText(), "M");
@@ -99,51 +87,61 @@ public class WikiResultsPage extends WikiPage {
 				
 			}
 		}
+		*/
+		return null;
 	}
 	
 	public Developer getDeveloper() {
+		/*
 		String designerS;
 		if (designer.isEmpty())
 			designerS = "N/A";
 		else designerS = designer.get(0).getText();
-		
+
 		try {
 			return new Developer(developer.get(0).getText(), designerS);
-			
+
 		} catch (NoSuchElementException e1) {
-			
+
 			try {
 				stubbornElements = driver.findElements(By.xpath("//table[@class='infobox hproduct']//tr[3]/td"));
 				return new Developer(stubbornElement.getText(), designerS);
-				
+
 			} catch (NoSuchElementException e2) {
-				
+
 				stubbornElements = driver.findElements(By.xpath("//table[@class='infobox infobox vevent']//tr[contains(.,'Developers')]/td"));
 				return new Developer(stubbornElement.getText(), designerS);
 			}
 		}
+		*/
+		
+		return null;
 	}
 	
 	public List<System> getPlatforms() {
 		
-		List<System> systems = new ArrayList<>();
+		/*
+			List<System> systems = new ArrayList<>();
 		
 		try {
 			platforms.forEach(platform -> {
-				
+
 				if (!platform.getText().equals("") && !platform.getText().equals(" "))
 					systems.add(new System(platform.getText()));
 			});
 		} catch (NoSuchElementException e) {
 			stubbornElements = driver.findElements(By.xpath("//table[@class='infobox infobox vevent']//tr[contains(.,'Developers')]/td"));
-			
+
 		}
 		
 		return systems;
+		*/
+		return null;
 	}
 	
 	public List<Genre> getGenres() {
 		
+		/*
 		List<Genre> genreList = new ArrayList<>();
 		
 		genres.forEach(genre -> {
@@ -152,6 +150,8 @@ public class WikiResultsPage extends WikiPage {
 		});
 		
 		return genreList;
+		*/
+		return null;
 	}
 	
 	public String getImageSource() {
@@ -171,7 +171,6 @@ public class WikiResultsPage extends WikiPage {
 		return "https://" + src;
 	}
 	
-	// TODO: 12/5/16 Handle wiki pages that show "related" links.
-	// TODO: 12/5/16 Handle org.openqa.selenium.NoSuchElementException for each method.
+	// TODO: 12/5/16 Sit in the corner and cry.
 	
 }
