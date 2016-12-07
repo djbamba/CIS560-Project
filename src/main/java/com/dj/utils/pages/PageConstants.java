89c999fb9c4335409b38a082057ca163d35b0001
Daniel Bamba <djbamba@ksu.edu>
2016-12-07 17:43:17 -0600

    Adding new constants for xpaths.

--- src/main/java/com/dj/utils/pages/PageConstants.java
+++ src/main/java/com/dj/utils/pages/PageConstants.java
@@ -8,5 +8,13 @@ public interface PageConstants {
 	
 	String GOOGLE_URL = "http://www.google.com";
 	String WIKI_URL = "https://en.wikipedia.org/wiki/Main_Page";
-	String WIKI_MEDIA = "https://upload.wikimedia.org/";
+	String PUB_1 = "";
+	String PUB_2 = "";
+	String DES_1 = "";
+	String DES_2 = "";
+	String PLAT_1 = "";
+	String PLAT_2 = "";
+	String GEN_1 = "";
+	String GEN_2 = "";
+	String IMAGE_URL = "";
 }
