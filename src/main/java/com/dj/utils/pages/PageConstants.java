1447cb0ea995310a664eabe499b8323dfd198cea
A.J. Cabanatuan <allanjay808@ksu.edu>
2016-12-09 15:50:28 -0600

    accepting master changes due to conflicts

--- src/main/java/com/dj/utils/pages/PageConstants.java
+++ src/main/java/com/dj/utils/pages/PageConstants.java
@@ -1,13 +1,30 @@
 package com.dj.utils.pages;
 
-
 /**
- * Created by DJ on 11/28/16.
+ * Created String DJ on 11/28/16.
  */
 public interface PageConstants {
 	
 	String GOOGLE_URL = "http://www.google.com";
 	String WIKI_URL = "https://en.wikipedia.org/wiki/Main_Page";
-	String WIKI_MEDIA = "https://upload.wikimedia.org/";
-	String WIKI_COMPANY_PUBLISHER = "https://en.wikipedia.org/wiki/List_of_video_game_publishers";
+	
+	String INFO_BOX = ".//table[@class='infobox hproduct']";
+	
+	String DEV_1 = ".//table[@class='infobox hproduct']/tbody//th[contains(.,'Developer(s)')]";
+	String DEV_2 = ".//table[@class='infobox infobox vevent']/tbody//th[contains(.,'Developers')]";
+	
+	String PUB_1 = ".//table[@class='infobox hproduct']/tbody//th[contains(.,'Publisher(s)')]";
+	String PUB_2 = ".//table[@class='infobox infobox vevent']/tbody//th[contains(.,'Publishers')]";
+	
+	String DES_1 = ".//table[@class='infobox hproduct']/tbody//th[contains(.,'Designer(s)')]";
+	String DES_2 = ".//table[@class='infobox infobox vevent']/tbody//th[contains(.,'Designers')]";
+	
+	String PLAT_1 = ".//table[@class='infobox hproduct']/tbody//th[contains(.,'Platform(s)')]";
+	String PLAT_2 = ".//table[@class='infobox infobox vevent']/tbody//th[contains(.,'Platforms')]";
+	
+	String GEN_1 = ".//table[@class='infobox hproduct']/tbody//th[contains(.,'Genre(s)')]";
+	String GEN_2 = ".//table[@class='infobox infobox vevent']/tbody//th[contains(.,'Genres')]";
+	
+	String IMAGE_PATH = ".//table[@class='infobox hproduct']//tr/td/a/img";
+	
 }
--- src/test/java/com/dj/test/Test.java
+++ src/test/java/com/dj/test/Test.java
@@ -1,5 +1,8 @@
 package com.dj.test;
 
+import com.dj.model.Developer;
+import com.dj.model.Genre;
+import com.dj.model.Publisher;
 import com.dj.model.System;
 import com.dj.utils.pages.WikiPage;
 import com.dj.utils.pages.WikiResultsPage;
@@ -8,11 +11,9 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -33,14 +34,14 @@ public class Test {
 	private static WebDriver driver;
 	
 	private static WikiPage wikiPage;
-	
-	private static WikiResultsPage wikiResults;
+
+//	private static WikiResultsPage wikiResults;
 	
 	@BeforeClass
 	public static void setup() {
 		java.lang.System.setProperty("webdriver.firefox.bin", "/Applications/Firefox-2.app/Contents/MacOS/firefox-bin");
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		wikiPage = new WikiPage(driver);
 	}
@@ -49,36 +50,93 @@ public class Test {
 	public static void tearDown() {
 		driver.close();
 	}
-
-	@Ignore
-	@org.junit.Test
-	public static void testSystemExtraction() {
-		wikiResults = wikiPage.searchGame("Far Cry Primal").getWikiResultsPage();
-		List<System> systems = wikiResults.getPlatforms();
-		
+	
+	public static void systemExtraction(WikiResultsPage wikiResults) {
 		try {
+			List<System> systems = wikiResults.getPlatforms();
 			
-			systems.stream().forEach(system -> {
+			systems.forEach(system -> {
 				LOG.info(system.toString());
 			});
 			
 		} catch (Exception e) {
-			LOG.error("testSystemExtraction: {}", e);
-		}
-		
-	}
-
-	@Ignore
-	@org.junit.Test
-	@FileParameters(value = "src/main/resources/data/games.csv", mapper = CsvWithHeaderMapper.class)
-	public static void systemTest(String id, String name, String moday,String year, String url) {
-		List<System> systems;
-		
-		try {
-			LOG.info("id: {} name: {} mmm-dd: {} year: {} url: {}", id, name, moday,year, url);
-		} catch (Exception e) {
-			LOG.error("Exception in systemTest", e);
+			LOG.error("Error in systemExtraction:", e);
 		}
 	}
 	
+	public static void developerExtraction(WikiResultsPage wikiResults) {
+		try {
+			Developer developer = wikiResults.getDeveloper();
+			LOG.info(developer.toString());
+			
+		} catch (Exception e) {
+			LOG.error("Error in developerExtraction:", e);
+		}
+	}
+	
+	public static void designerExtraction(WikiResultsPage wikiResultsPage) {
+		try {
+			String designer = wikiResultsPage.getDesigner();
+			LOG.info("Designer: {}", designer);
+		} catch (Exception e) {
+			LOG.error("Error in designerExtraction:", e);
+		}
+	}
+	
+	public static void genreExtraction(WikiResultsPage wikiResults) {
+		try {
+			List<Genre> genres = wikiResults.getGenres();
+			
+			genres.forEach(genre -> {
+				LOG.info(genre.toString());
+			});
+			
+		} catch (Exception e) {
+			LOG.error("Error in genreExtraction:", e);
+		}
+	}
+	
+	public static void urlExtraction(WikiResultsPage wikiResults) {
+		try {
+			String url = wikiResults.getImageSource();
+			LOG.info("URL: {}", url);
+		} catch (Exception e) {
+			LOG.error("Error in urlExtraction:", e);
+		}
+	}
+	
+	public static void publisherExtraction(WikiResultsPage wikiResults) {
+		try {
+			Publisher publisher = wikiResults.getPublisher();
+			LOG.info(publisher.toString());
+			
+		} catch (Exception e) {
+			LOG.error("Error in publisherExtraction:", e);
+		}
+	}
+	
+	@org.junit.Test
+	@FileParameters(value = "src/main/resources/data/games.csv", mapper = CsvWithHeaderMapper.class)
+	public static void gameInfoExtractionTest(String id, String name, String date, String url) {
+		
+		try {
+			LOG.info("id: {} name: {} date: {} url: {}", id, name, date, url);
+			WikiResultsPage wikiResults = wikiPage.searchGame(name).getWikiResultsPage();
+			// test systems extraction
+			systemExtraction(wikiResults);
+			// test developer extraction
+			developerExtraction(wikiResults);
+			//test designer extraction
+			designerExtraction(wikiResults);
+			// test genre extraction
+			genreExtraction(wikiResults);
+			// test url extraction
+			urlExtraction(wikiResults);
+			// test publisher extraction
+			publisherExtraction(wikiResults);
+		} catch (Exception e) {
+			LOG.error("Exception in gameInfoExtractionTest", e);
+		}
+	}
+	// check Grand Theft Auto IV genres, BioShock, The Elder Scrolls V: Skyrim
 }
