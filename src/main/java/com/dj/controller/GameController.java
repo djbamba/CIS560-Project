8b2ef56d79c783253f218532efe2c232bad31c40
Zachary Terwort <zterwort@ksu-wireless-5-74.campus.ksu.edu>
2016-12-11 14:02:51 -0600

    Fixed searched.html to work with thymeleaf

--- src/main/java/com/dj/controller/GameController.java
+++ src/main/java/com/dj/controller/GameController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -41,5 +42,10 @@ public class GameController {
 		
 		return "games/game";
 	}
-	
+
+	@RequestMapping(value = "recent", method = RequestMethod.GET)
+	public String getRecentGames(@RequestParam("searchText")String searchText, Model model){
+		model.addAttribute("games", gameRepository.findByNameLike(searchText));
+		return "games/game";
+	}
 }
--- src/main/java/com/dj/controller/GenreController.java
+++ src/main/java/com/dj/controller/GenreController.java
@@ -4,5 +4,6 @@ package com.dj.controller;
  * Created by DJ on 11/23/16.
  */
 public class GenreController {
-	
+
+
 }
