cc37f0c8c899b2ef4d805749b3f7b69b81cc6b85
Daniel Bamba <djbamba@ksu.edu>
2016-12-11 13:09:42 -0600

    Added findByName methods to all repositories.

--- src/main/java/com/dj/repository/DeveloperRepository.java
+++ src/main/java/com/dj/repository/DeveloperRepository.java
@@ -12,4 +12,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DeveloperRepository extends JpaRepository<Developer, Integer> {
 	
+	Developer findByName(String name);
 }
--- src/main/java/com/dj/repository/GenreRepository.java
+++ src/main/java/com/dj/repository/GenreRepository.java
@@ -12,4 +12,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Integer> {
 	
+	Genre findByName(String name);
 }
