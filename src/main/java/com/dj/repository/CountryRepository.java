b9d1464df79c2493bba605ba26ecb0e1c91b11db
Daniel Bamba <djbamba@ksu.edu>
2016-12-11 22:59:02 -0600

    Added sql command to populate table from csv file as a comment.

--- src/main/java/com/dj/repository/CountryRepository.java
+++ src/main/java/com/dj/repository/CountryRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 /**
  * Created by DJ on 11/23/16.
  */
-
+// INSERT INTO COUNTRY SELECT * FROM CSVREAD('./src/main/resources/data/countries2.csv');
 @Repository
 public interface CountryRepository extends JpaRepository<Country, String> {
 	
