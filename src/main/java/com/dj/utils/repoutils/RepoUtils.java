638e3025396c3c08ccc764a59922d9752ec7aefd
Daniel Bamba <djbamba@ksu.edu>
2016-12-11 18:27:07 -0600

    Re-org'd RepoUtils.

--- src/main/java/com/dj/utils/repoutils/RepoUtils.java
+++ src/main/java/com/dj/utils/repoutils/RepoUtils.java
@@ -1,9 +1,11 @@
 package com.dj.utils.repoutils;
 
+import com.dj.model.Company;
 import com.dj.model.Country;
 import com.dj.model.Genre;
 import com.dj.model.ScoreWebsite;
 import com.dj.model.System;
+import com.dj.repository.CompanyRepository;
 import com.dj.repository.CountryRepository;
 import com.dj.repository.GenreRepository;
 import com.dj.repository.ScoreWebsiteRepository;
@@ -17,32 +19,25 @@ import java.util.List;
  */
 public class RepoUtils {
 	
-	public static System checkSystem(System system, SystemRepository systemRepository) {
-		System temp;
-		if (systemRepository.findByName(system.getName()) == null) {
-			temp = systemRepository.save(system);
-			return temp;
+	public static Company checkCompany(Company company, CompanyRepository companyRepository) {
+		if (companyRepository.findByName(company.getName()) == null) {
+			return companyRepository.save(company);
 		}
-		return systemRepository.findByName(system.getName());
+		return companyRepository.findByName(company.getName());
 	}
 	
-	public static List<System> checkSystems(List<System> systems, SystemRepository systemRepository) {
-		List<System> temps = new ArrayList<>();
-		
-		for (System system : systems) {
-			if (systemRepository.findByName(system.getName()) == null) {
-				temps.add(systemRepository.save(system));
-			}
-			temps.add(systemRepository.findByName(system.getName()));
+	public static Country checkCountry(Country country, CountryRepository countryRepository) {
+		if (countryRepository.findByCode(country.getCode()) == null) {
+			return countryRepository.save(country);
 		}
-		return temps;
+		return countryRepository.findByCode(country.getCode());
 	}
+	
+	// TODO: 12/11/16 developer, game
 	
 	public static Genre checkGenre(Genre genre, GenreRepository genreRepository) {
-		Genre temp;
 		if (genreRepository.findByGenre(genre.getGenre()) == null) {
-			temp = genreRepository.save(genre);
-			return temp;
+			return genreRepository.save(genre);
 		}
 		return genreRepository.findByGenre(genre.getGenre());
 	}
@@ -57,23 +52,32 @@ public class RepoUtils {
 		}
 		return temps;
 	}
+	// TODO: 12/11/16 publisher, purchaseWebsite
 	
-	public static Country checkCountry(Country country, CountryRepository countryRepository) {
-		Country temp;
-		if (countryRepository.findByName(country.getName()) == null) {
-			temp = countryRepository.save(country);
-			return temp;
+	public static System checkSystem(System system, SystemRepository systemRepository) {
+		if (systemRepository.findByName(system.getName()) == null) {
+			return systemRepository.save(system);
 		}
-		return countryRepository.findByName(country.getName());
+		return systemRepository.findByName(system.getName());
 	}
 	
 	public static ScoreWebsite checkScoreWebsite(ScoreWebsite scoreWebsite, ScoreWebsiteRepository scoreWebsiteRepository) {
-		ScoreWebsite temp;
 		if (scoreWebsiteRepository.findByName(scoreWebsite.getName()) == null) {
-			temp = scoreWebsiteRepository.save(scoreWebsite);
-			return temp;
+			return scoreWebsiteRepository.save(scoreWebsite);
 		}
 		return scoreWebsiteRepository.findByName(scoreWebsite.getName());
+	}
+	
+	public static List<System> checkSystems(List<System> systems, SystemRepository systemRepository) {
+		List<System> temps = new ArrayList<>();
+		
+		for (System system : systems) {
+			if (systemRepository.findByName(system.getName()) == null) {
+				temps.add(systemRepository.save(system));
+			}
+			temps.add(systemRepository.findByName(system.getName()));
+		}
+		return temps;
 	}
 	
 }
