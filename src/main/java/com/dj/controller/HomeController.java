a61cff642f8d260649f92bd00277f0cb93b18c60
Zachary Terwort <zterwort@ksu-wireless-5-74.campus.ksu.edu>
2016-12-11 20:42:39 -0600

    Fixed all controllers/Created all html pages

--- src/main/java/com/dj/controller/HomeController.java
+++ src/main/java/com/dj/controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.dj.controller;
 
+import com.dj.repository.GenreRepository;
+import com.dj.repository.SystemRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -12,9 +16,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     private static final Logger LOG = LogManager.getLogger(HomeController.class);
-    
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    @Autowired
+    private SystemRepository systemRepository;
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("genres", genreRepository.findAll());
         LOG.info("Index requested");
         return "index";
     }
