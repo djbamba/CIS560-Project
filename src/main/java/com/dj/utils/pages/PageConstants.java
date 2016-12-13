a568e17d0edd058fcb48de6f71ab534f93917602
Daniel Bamba <djbamba@ksu.edu>
2016-12-13 03:57:51 -0600

    Added constant for Bing's game ads.

--- src/main/java/com/dj/utils/pages/PageConstants.java
+++ src/main/java/com/dj/utils/pages/PageConstants.java
@@ -5,6 +5,7 @@ package com.dj.utils.pages;
  */
 public interface PageConstants {
 	
+	String PURCHASE_SITES = ".//li[@class='b_ad']//ul[@class='b_hList']/li";
 	String GOOGLE_URL = "http://www.google.com";
 	String WIKI_URL = "https://en.wikipedia.org/wiki/Main_Page";
 	String WIKI_COMPANY_PUBLISHER = "https://en.wikipedia.org/wiki/List_of_video_game_publishers";
